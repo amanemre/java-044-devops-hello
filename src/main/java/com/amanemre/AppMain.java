698f3b6b0090428a9055bbb734795e7219899754
EMRE AMAN <emreaman@EMREs-MacBook-Pro.local>
2024-08-31 15:00:41 +0300

    first commit

---
+++ src/main/java/com/amanemre/AppMain.java
@@ -0,0 +1,14 @@
+package com.amanemre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppMain.class, args);
+    System.out.println("Hello Devops");
+  }
+}
